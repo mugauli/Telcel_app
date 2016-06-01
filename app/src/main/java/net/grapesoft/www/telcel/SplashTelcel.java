df2f5620ea360191cf194c0a2220fe8faadb0c33
Mugauli <mugauli2@hotmail.com>
2016-06-01 11:04:45 -0500

    SPLASH TERMINADO UMG

---
+++ app/src/main/java/net/grapesoft/www/telcel/SplashTelcel.java
@@ -0,0 +1,40 @@
+package net.grapesoft.www.telcel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Mugauli on 01/06/2016.
+ */
+public class SplashTelcel extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashTelcel.this,login.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
