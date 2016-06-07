6354e61fe786c71113a0f331bc913df16e686b1e
Mugauli <mugauli2@hotmail.com>
2016-06-07 01:17:50 -0500

    Toolbar

--- app/src/main/java/Utitilies/Comunication.java
+++ app/src/main/java/Utitilies/Comunication.java
@@ -17,6 +17,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
