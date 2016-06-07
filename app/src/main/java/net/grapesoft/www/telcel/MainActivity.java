de0e1ffc508f1642f9d55952d700a0f76a352061
Mugauli <mugauli2@hotmail.com>
2016-06-07 08:52:45 -0500

    navigation view telcel

--- app/src/main/java/net/grapesoft/www/telcel/MainActivity.java
+++ app/src/main/java/net/grapesoft/www/telcel/MainActivity.java
@@ -1,5 +1,6 @@
 package net.grapesoft.www.telcel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -82,6 +83,9 @@ public class MainActivity extends AppCompatActivity
 
 
         if (id == R.id.nav_camera) {
+            Intent i = new Intent(MainActivity.this, ActualizarActivity.class);
+            startActivity(i);
+            finish();
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
