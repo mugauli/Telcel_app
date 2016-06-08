b2c650c3571f9ffbe5d059602e09936778e7ca68
Mugauli <mugauli2@hotmail.com>
2016-06-08 03:50:23 -0500

    navigation view telcel (imagenes)

--- app/src/main/java/net/grapesoft/www/telcel/enviado.java
+++ app/src/main/java/net/grapesoft/www/telcel/enviado.java
@@ -1,16 +1,32 @@
 package net.grapesoft.www.telcel;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class enviado extends ActionBarActivity {
+import Utitilies.SessionManagement;
+
+public class enviado extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    SessionManagement session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enviado);
+
+        session = new SessionManagement(getApplicationContext());
 
         //Fuentes
         TextView txtGhost = (TextView) findViewById(R.id.textView11);
@@ -22,5 +38,71 @@ public class enviado extends ActionBarActivity {
         // Applying font
         txtGhost.setTypeface(tf);
         txtGhost1.setTypeface(tf);
+
+
+//Toolbar Menu
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.telcelnosune);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        ImageButton imgButton = (ImageButton) findViewById(R.id.btnMenu);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //ToolBar Menu
     }
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_camera) {
+            Intent i = new Intent(enviado.this, ActualizarActivity.class);
+            startActivity(i);
+
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent i = new Intent(enviado.this, pin.class);
+            startActivity(i);
+
+
+        } else if (id == R.id.nav_slideshow) {
+            Intent i = new Intent(enviado.this, preferencias.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_send) {
+            session.logoutUser();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
