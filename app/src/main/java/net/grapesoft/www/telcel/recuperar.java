15a0a0c0507fae69ce3cac092806e513238290c3
Mugauli <mugauli2@hotmail.com>
2016-06-08 03:58:12 -0500

    barra de menu

--- app/src/main/java/net/grapesoft/www/telcel/recuperar.java
+++ app/src/main/java/net/grapesoft/www/telcel/recuperar.java
@@ -2,21 +2,33 @@ package net.grapesoft.www.telcel;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class recuperar extends ActionBarActivity {
+import Utitilies.SessionManagement;
 
+public class recuperar extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    SessionManagement session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar);
+
+        session = new SessionManagement(getApplicationContext());
 
         TextView texto_superior_entrada = (TextView) findViewById(R.id.textView13);
         TextView texto_superior = (TextView) findViewById(R.id.textView14);
@@ -38,7 +50,74 @@ public class recuperar extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+
+
+//Toolbar Menu
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.telcelnosune);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        ImageButton imgButton = (ImageButton) findViewById(R.id.btnMenu);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //ToolBar Menu
     }
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_camera) {
+            Intent i = new Intent(recuperar.this, ActualizarActivity.class);
+            startActivity(i);
+
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent i = new Intent(recuperar.this, pin.class);
+            startActivity(i);
+
+
+        } else if (id == R.id.nav_slideshow) {
+            Intent i = new Intent(recuperar.this, preferencias.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_send) {
+            session.logoutUser();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
