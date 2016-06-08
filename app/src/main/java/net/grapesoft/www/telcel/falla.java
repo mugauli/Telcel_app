86b18d90a499c1d7b22708acc867597a05d65127
Mugauli <mugauli2@hotmail.com>
2016-06-08 04:11:47 -0500

    barra de menu

--- app/src/main/java/net/grapesoft/www/telcel/falla.java
+++ app/src/main/java/net/grapesoft/www/telcel/falla.java
@@ -1,33 +1,47 @@
 package net.grapesoft.www.telcel;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import java.util.ArrayList;
 
 import android.app.Activity;
 
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import Utitilies.SessionManagement;
 
-public class falla extends ActionBarActivity {
+
+public class falla extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    SessionManagement session;
+
     final Context context = this;
     private ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_falla);
-
+        session = new SessionManagement(getApplicationContext());
         TextView txtGhost = (TextView) findViewById(R.id.textView_desc1);
         TextView txtGhost2 = (TextView) findViewById(R.id.textView_desc2);
         TextView txtGhost3 = (TextView) findViewById(R.id.textView_comentario);
@@ -73,7 +87,70 @@ public class falla extends ActionBarActivity {
 
         });
 
+//Toolbar Menu
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.telcelnosune);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        ImageButton imgButton = (ImageButton) findViewById(R.id.btnMenu);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //ToolBar Menu
     }
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_camera) {
+            Intent i = new Intent(falla.this, ActualizarActivity.class);
+            startActivity(i);
+
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent i = new Intent(falla.this, pin.class);
+            startActivity(i);
+
+
+        } else if (id == R.id.nav_slideshow) {
+            Intent i = new Intent(falla.this, preferencias.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_send) {
+            session.logoutUser();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/net/grapesoft/www/telcel/sugerencias.java
+++ app/src/main/java/net/grapesoft/www/telcel/sugerencias.java
@@ -2,21 +2,36 @@ package net.grapesoft.www.telcel;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class sugerencias extends ActionBarActivity {
+import Utitilies.SessionManagement;
+
+public class sugerencias extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    SessionManagement session;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sugerencias);
+
+        session = new SessionManagement(getApplicationContext());
 
         TextView txtGhost = (TextView) findViewById(R.id.textView5);
         TextView txtGhost2 = (TextView) findViewById(R.id.textView6);
@@ -31,7 +46,74 @@ public class sugerencias extends ActionBarActivity {
         txtGhost2.setTypeface(tf);
         txtGhost3.setTypeface(tf);
         btn.setTypeface(tf);
+
+
+//Toolbar Menu
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.telcelnosune);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        ImageButton imgButton = (ImageButton) findViewById(R.id.btnMenu);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //ToolBar Menu
     }
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_camera) {
+            Intent i = new Intent(sugerencias.this, ActualizarActivity.class);
+            startActivity(i);
+
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent i = new Intent(sugerencias.this, pin.class);
+            startActivity(i);
+
+
+        } else if (id == R.id.nav_slideshow) {
+            Intent i = new Intent(sugerencias.this, preferencias.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_send) {
+            session.logoutUser();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
