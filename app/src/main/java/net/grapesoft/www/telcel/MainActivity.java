385b59d1abfb829bf101559037cc489c25014aa5
Mugauli <mugauli2@hotmail.com>
2016-06-08 02:27:34 -0500

    navigation view telcel (imagenes)

--- app/src/main/java/net/grapesoft/www/telcel/MainActivity.java
+++ app/src/main/java/net/grapesoft/www/telcel/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity
 
         //boton ayuda
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
