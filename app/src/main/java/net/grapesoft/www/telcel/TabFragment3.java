1957e576844bbe060425f3e5badf24fb800c8195
memobluehack <guillermo.rodriguez@grapesoft.net>
2016-06-16 13:43:26 -0500

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_login.xml
    
    Ultima version - actualizacion de logon

---
+++ app/src/main/java/net/grapesoft/www/telcel/TabFragment3.java
@@ -0,0 +1,20 @@
+package net.grapesoft.www.telcel;
+
+/**
+ * Created by memoHack on 14/06/2016.
+ */
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+    }
+}
