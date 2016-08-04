2d7151b4bdec4d91da4f284f3c275e24a31e1b39
memobluehack <guillermo.rodriguez@grapesoft.net>
2016-08-04 03:37:08 -0500

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_login.xml
    
    Ultima version - comunicacion interna design

--- app/src/main/java/net/grapesoft/www/telcel/activity_respuesta.java
+++ app/src/main/java/net/grapesoft/www/telcel/activity_respuesta.java
@@ -1,8 +1,19 @@
 package net.grapesoft.www.telcel;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -15,15 +26,88 @@ import org.json.JSONException;
 import java.util.ArrayList;
 
 import Utitilies.PreguntaElement;
+import Utitilies.SessionManagement;
 
-public class activity_respuesta extends AppCompatActivity {
+public class activity_respuesta extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     JSONArray responseArray2;
-
+    SessionManagement session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_respuesta);
+
+        session = new SessionManagement(getApplicationContext());
+
+        //boton ayuda
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity_respuesta.this,ayuda.class);
+                startActivity(intent);
+            }
+        });
+        Typeface tfm = Typeface.createFromAsset(getAssets(), "fonts/media.otf");
+        TextView txtGhost4 = (TextView) findViewById(R.id.TitleSeccion);
+        txtGhost4.setTypeface(tfm);
+        txtGhost4.setText("PREGUNTA DEL DIA");
+        //boton ayuda
+
+        //Toolbar Menu
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.telcelnosune);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        if (toolbar != null) {
+            toolbar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(MainActivity.this,"Toolbar title clicked",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(activity_respuesta.this, MainActivity.class);
+                    i.putExtra("direccion","0");
+                    startActivity(i);
+                }
+            });
+        }
+// Find logo
+
+        ImageButton imgButton = (ImageButton) findViewById(R.id.btnMenu);
+        ImageButton imgButton2 = (ImageButton) findViewById(R.id.btnTrivia);
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        if (imgButton != null) {
+            imgButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (drawer.isDrawerOpen(GravityCompat.START)) {
+                        drawer.closeDrawer(GravityCompat.START);
+                    } else {
+                        drawer.openDrawer(GravityCompat.START);
+                    }
+                }
+            });
+        }
+
+        if (imgButton2 != null) {
+            imgButton2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(activity_respuesta.this, triviasActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
 
         String pregunta = getIntent().getExtras().getString("pregunta", "No hay pregunta del día");
         String respuesta = getIntent().getExtras().getString("respuesta", "No hay respuesta del día");
@@ -56,5 +140,70 @@ public class activity_respuesta extends AppCompatActivity {
             txtRespuesta.setText("R: "+respuesta);
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_camera) {
+            Intent i = new Intent(activity_respuesta.this, ActualizarActivity.class);
+            startActivity(i);
+
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent i = new Intent(activity_respuesta.this, pin.class);
+            startActivity(i);
+
+
+        } else if (id == R.id.nav_slideshow) {
+            Intent i = new Intent(activity_respuesta.this, preferencias.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_send) {
+            session.logoutUser();
+            finish();
+            System.exit(0);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
