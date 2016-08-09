c4201b6f94f1782b11ea5505c49fcf40df8ea3fe
memobluehack <guillermo.rodriguez@grapesoft.net>
2016-08-09 17:09:27 -0500

    Merge remote-tracking branch 'origin/master' into Respaldo
    # Conflicts:
    #	app/src/main/java/Utitilies/Lista_Entrada.java

--- app/src/main/java/net/grapesoft/www/telcel/FragmentSitiosInteres.java
+++ app/src/main/java/net/grapesoft/www/telcel/FragmentSitiosInteres.java
@@ -20,6 +20,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,7 +39,7 @@ public class FragmentSitiosInteres extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View rootview = inflater.inflate(R.layout.activity_sitios, container, false);
+        final View rootview = inflater.inflate(R.layout.tab_fragment_sitios, container, false);
 
         tokenCTE = getText(R.string.tokenXM).toString();
 
@@ -46,7 +47,93 @@ public class FragmentSitiosInteres extends Fragment {
 
         ArrayList<Lista_Entrada> datos = new ArrayList<Lista_Entrada>();
 
-        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.logoaprende,getString(R.string.aprende)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.aprende,R.drawable.logoaprende,getString(R.string.aprende)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.claromusica,R.drawable.logoclaromusica,getString(R.string.clarom)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.clarovideo,R.drawable.logoclarovideo,getString(R.string.clarov)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.clickisalud,R.drawable.logoclickisalud,getString(R.string.salud)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.holatelcel,R.drawable.logoholatelcel,getString(R.string.holatel)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.naturaleza,R.drawable.logonaturaleza,getString(R.string.naturaleza)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.racing,R.drawable.logoracing,getString(R.string.racing)));
+        datos.add(new Lista_Entrada(R.drawable.arrow,R.drawable.telcelred,R.drawable.logotelcelred,getString(R.string.prox)));
+
+        //COdigo
+        lista = (ListView) rootview.findViewById(R.id.sitioslista);
+
+
+        lista.setAdapter(new List_adapted(rootview.getContext(), R.layout.entrada_sitios, datos){
+            @Override
+            public void onEntrada(Object entrada, View view) {
+               if (entrada != null) {
+                    // Applying font
+
+
+                    TextView texto_inferior_entrada = (TextView) view.findViewById(R.id.textView_inferior);
+                    Typeface tfi = Typeface.createFromAsset(view.getContext().getAssets(), "fonts/ligera.otf");
+                    texto_inferior_entrada.setTypeface(tfi);
+                    if (texto_inferior_entrada != null)
+                        texto_inferior_entrada.setText(((Lista_Entrada) entrada).get_textoDebajo());
+
+                    ImageView imagen_entrada = (ImageView) view.findViewById(R.id.sitios);
+                    if (imagen_entrada != null)
+                        imagen_entrada.setImageResource(((Lista_Entrada) entrada).get_idImagenDetalle());//
+
+                    ImageView imagen_entradad = (ImageView) view.findViewById(R.id.sitioslogo);
+                    if (imagen_entradad != null)
+                        imagen_entradad.setImageResource(((Lista_Entrada) entrada).get_idImagen());//get_idImagenDetalle()
+                    ImageView imagen_entradaa = (ImageView) view.findViewById(R.id.arrow);
+                    if (imagen_entradaa != null)
+                        imagen_entradaa.setImageResource(((Lista_Entrada) entrada).get_idImagenA());//get_idImagenDetalle()
+                }
+            }
+        });
+
+      lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> pariente, View view, int posicion, long id) {
+                Lista_Entrada elegido = (Lista_Entrada) pariente.getItemAtPosition(posicion);
+
+                CharSequence texto = "Seleccionado: " + elegido.get_textoDebajo();
+
+                if (elegido.get_textoDebajo() == getString(R.string.aprende)) {
+
+                    Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://aprende.org/pages.php?r=.index"));
+                    startActivity(intent);
+
+                } else if (elegido.get_textoDebajo() == getString(R.string.clarom)) {
+                    Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("http://www.claromusica.com/intro"));
+                    startActivity(intent);
+
+
+                } else if (elegido.get_textoDebajo() == getString(R.string.clarov)) {
+                    Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.clarovideo.com/mexico/homeuser"));
+                    startActivity(intent);
+
+                } else if (elegido.get_textoDebajo() == getString(R.string.salud)) {
+                    Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.clikisalud.net/"));
+                    startActivity(intent);
+
+                } else if (elegido.get_textoDebajo() == getString(R.string.holatel)) {
+                    Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("http://holatelcel.com/"));
+                    startActivity(intent);
+
+
+                } else if (elegido.get_textoDebajo() == getString(R.string.naturaleza)) {
+                    Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("http://lanaturalezanosllama.com/"));
+                    startActivity(intent);
+
+
+                } else if (elegido.get_textoDebajo() == getString(R.string.racing)) {
+                    Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("http://telcelracing.com/v2/"));
+                    startActivity(intent);
+                } else if (elegido.get_textoDebajo() == getString(R.string.prox)) {
+                    /*Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("http://internetencaja.com.mx/telcel-registro/"));
+                    startActivity(intent);*/
+                }
+            }
+
+      });
+
+        //codigo
 
 
 
